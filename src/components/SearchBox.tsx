c4aca2487f2cb135979466fca2c25185acd10311
moonR2 <moonyneverend@gmail.com>
2023-11-15 17:11:19 -0500

    SearchResults component done

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -4,6 +4,8 @@ import usePlacesAutocomplete, {
 } from "use-places-autocomplete";
 import { ChangeEvent, useEffect } from "react";
 import { HiMapPin } from "react-icons/hi2";
+import { useState } from "react";
+import SearchResults from "./SearchResults";
 
 const SearchBox = () => {
   const defaultValue = "875";
@@ -21,10 +23,12 @@ const SearchBox = () => {
     suggestions: { status, data },
     clearSuggestions,
   } = usePlacesAutocomplete({ debounce: 300, defaultValue });
+  const [selectedResult, setSelectedResult] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     setValue(e.target.value);
+    setSelectedResult(null); // Limpiar el resultado seleccionado cuando se cambia la entrada
     if (e.target.value === "") {
       onSelectAddress("", null, null);
     }
@@ -48,20 +52,29 @@ const SearchBox = () => {
   };
 
   return (
-    <div
-      className="bg-white p-4 shadow-md border-gray-200 border"
-      style={{ width: "650px" }}
-    >
-      <div className="relative flex items-center">
-        <input
-          value={value}
-          onChange={handleChange}
-          className="w-full h-12 px-4 pl-12 text-black"
-        />
-        <span className="absolute left-1 top-2">
-          <HiMapPin className="text-black text-2xl" />
-        </span>
+    <div className="flex flex-col">
+      <div
+        className="bg-white p-4 shadow-md border-gray-200 border"
+        style={{ width: "650px" }}
+      >
+        <div className="relative flex items-center">
+          <input
+            value={value}
+            onChange={handleChange}
+            className="w-full h-12 px-4 pl-12 text-black outline-none focus:outline-none bg-transparent"
+          />
+          <span className="absolute left-1 top-2">
+            <HiMapPin className="text-black text-2xl" />
+          </span>
+        </div>
       </div>
+      <SearchResults
+        results={data || []}
+        onSelectResult={(placeId) => {
+          setSelectedResult(placeId);
+          // Puedes realizar acciones adicionales con el resultado seleccionado si es necesario
+        }}
+      />
     </div>
   );
 };
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,53 @@
+import { FiMapPin } from "react-icons/fi";
+
+interface SearchResult {
+  description: string;
+  place_id: string;
+  structured_formatting: {
+    main_text: string;
+    secondary_text: string;
+  };
+}
+
+interface SearchResultsProps {
+  results: SearchResult[];
+  onSelectResult: (placeId: string) => void;
+}
+
+const SearchResults = ({ results, onSelectResult }: SearchResultsProps) => {
+  if (results.length === 0) {
+    return null; // No hay resultados, no renderiza nada
+  }
+
+  return (
+    <ul className="list-none p-0 m-0">
+      {results.map((result) => (
+        <div
+          key={result.place_id}
+          className="bg-white p-4 shadow-md border-gray-200 border max-h-40 hover:bg-gray-200"
+          style={{ width: "650px" }}
+        >
+          <div className="relative flex items-center">
+            <li
+              key={result.place_id}
+              className="cursor-pointer py-2 px-8 ml-4"
+              onClick={() => onSelectResult(result.place_id)}
+            >
+              <div className="text-black">
+                {result.structured_formatting.main_text}
+              </div>
+              <div className="text-gray-500 text-sm">
+                {result.structured_formatting.secondary_text}
+              </div>
+            </li>
+            <span className="absolute left-1">
+              <FiMapPin className="text-gray-400 text-2xl" />
+            </span>
+          </div>
+        </div>
+      ))}
+    </ul>
+  );
+};
+
+export default SearchResults;
